2779e8e64c4ef63c9bf304b3663caa5c2f712a1a
Eric Schmitz <jan_eric.schmitz@th-wildau.de>
2019-05-28 20:57:03 +0200

    add .toml mqqt broker

--- action-getAddition-Taschenrechner.py
+++ action-getAddition-Taschenrechner.py
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from hermes_python.hermes import Hermes
+import toml
 
+
+USERNAME_INTENTS = "domi"
+MQTT_BROKER_ADDRESS = "localhost:1883"
+MQTT_USERNAME = None
+MQTT_PASSWORD = None
+
+
+def user_intent(intentname):
+    return USERNAME_INTENTS + ":" + intentname
 
 def action_wrapper(hermes, intent_message):
     first = int(intent_message.slots.firstTerm.first().value)
@@ -15,7 +25,15 @@ def action_wrapper(hermes, intent_message):
     current_session_id = intent_message.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
 
-
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("domi:getAddition", action_wrapper).start()
+    snips_config = toml.load('/etc/snips.toml')
+    if 'mqtt' in snips_config['snips-common'].keys():
+        MQTT_BROKER_ADDRESS = snips_config['snips-common']['mqtt']
+    if 'mqtt_username' in snips_config['snips-common'].keys():
+        MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
+    if 'mqtt_password' in snips_config['snips-common'].keys():
+        MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+
+    mqtt_opts = MqttOptions(username=MQTT_USERNAME, password=MQTT_PASSWORD, broker_address=MQTT_BROKER_ADDRESS)
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intents("domi:getAddition", action_wrapper).start()
